b0ecf9ad365c61fcf2324a675ae2149789375c2b
Stillrem <106412413+Stillrem@users.noreply.github.com>
2024-11-11 15:51:34 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,8 +6,6 @@
          return cache.addAll([
            '/',
            '/index.html',
-           '/styles.css',
-           '/script.js',
            '/icon-192x192.png',
            '/icon-512x512.png'
          ]);
