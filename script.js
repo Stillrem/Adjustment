1b56f2fd10b0d90d0e7783a8973444c55124c059
Stillrem <106412413+Stillrem@users.noreply.github.com>
2024-11-12 00:51:01 -0800

    Update script.js

--- script.js
+++ script.js
@@ -19,14 +19,12 @@ function calculatePay() {
         }
 
 // Регистрация service worker
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-            .then((registration) => {
-                console.log('Service Worker registered with scope:', registration.scope);
-            })
-            .catch((error) => {
-                console.error('Service Worker registration failed:', error);
-            });
-    });
-}
+            if ('serviceWorker' in navigator) {
+                navigator.serviceWorker.register('/service-worker.js')
+                .then(registration => {
+                    console.log('Service Worker registered with scope:', registration.scope);
+                })
+                .catch(error => {
+                    console.error('Service Worker registration failed:', error);
+                });
+            }
