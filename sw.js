14df19bebd41de091b5aaad5a3a6aa7b2554aa30
Stillrem <106412413+Stillrem@users.noreply.github.com>
2024-11-11 23:17:56 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,22 @@
-      // sw.js
-   self.addEventListener('install', function(event) {
-     // Кэширование файлов
-     event.waitUntil(
-       caches.open('my-cache').then(function(cache) {
-         return cache.addAll([
-           '/',
-           '/index.html',
-           '/icon-192x192.png',
-           '/icon-512x512.png'
-         ]);
-       })
-     );
-   });
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('pay-calculator-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/manifest.json',
+        '/icon-192x192.png',
+        '/icon-512x512.png'
+      ]);
+    })
+  );
+});
 
-   self.addEventListener('fetch', function(event) {
-     event.respondWith(
-       caches.match(event.request).then(function(response) {
-         return response || fetch(event.request);
-       })
-     );
-   });
-   
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
