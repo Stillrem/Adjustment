e346872c2867d04a67cf394e7e56d4595b74602e
Stillrem <106412413+Stillrem@users.noreply.github.com>
2024-11-12 00:34:26 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function calculatePay() {
-    const hours = parseFloat(document.getElementById('hours').value) || 0;
-    const minutes = parseFloat(document.getElementById('minutes').value) || 0;
-    const basePay = parseFloat(document.getElementById('basePay').value) || 0;
-  
-    const pay = 23 * (hours + (minutes * 1.6666 / 100)) - basePay;
-    document.getElementById('result').innerText = 'Adjustment: ${pay.toFixed(2)}`;
-}
+            const hours = parseFloat(document.getElementById('hours').value) || 0;
+            const minutes = parseFloat(document.getElementById('minutes').value) || 0;
+            const basePay = parseFloat(document.getElementById('basePay').value) || 0;
+          
+            const pay = 23 * (hours + (minutes * 1.6666 / 100)) - basePay;
+            document.getElementById('result').innerText = `Результат: ${pay.toFixed(2)}`;
+        }
 
-function toggleInput() {
-    const inputContainer = document.getElementById('input-container');
-    if (inputContainer.classList.contains('visible')) {
-        inputContainer.classList.remove('visible');
-        inputContainer.classList.add('hidden');
-    } else {
-        inputContainer.classList.remove('hidden');
-        inputContainer.classList.add('visible');
-    }
-}
+        function toggleInput() {
+            const inputContainer = document.getElementById('input-container');
+            if (inputContainer.classList.contains('visible')) {
+                inputContainer.classList.remove('visible');
+                inputContainer.classList.add('hidden');
+            } else {
+                inputContainer.classList.remove('hidden');
+                inputContainer.classList.add('visible');
+            }
+        }
 
 // Регистрация service worker
 if ('serviceWorker' in navigator) {
