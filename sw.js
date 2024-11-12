8603d150d6669ade29fd2f8d89bcd5db2e71ce21
Stillrem <106412413+Stillrem@users.noreply.github.com>
2024-11-12 00:05:50 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,49 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('pay-calculator-cache').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/script.js',
-        '/style.css',
-        '/manifest.json',
-        '/sw.js',
-        '/icon-192x192.png',
-        '/icon-512x512.png'
-      ]);
-    })
-  );
+const cacheName = 'app-cache-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', event => {
+  self.clients.claim();
 });
+
+self.addEventListener('fetch', async event => {
+  const req = event.request;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    event.respondWith(cacheFirst(req));
+  } else {
+    event.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
